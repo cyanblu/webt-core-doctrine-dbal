928cbc98f109c4b85b4ed9d4452f14c57c43af96
Moritz Liedl <9186@htl.rennweg.at>
2023-04-12 13:06:18 +0200

    datenbank erstellt und fertig

---
+++ doctrine_rps.sql
@@ -0,0 +1,34 @@
+drop database if exists rps;
+create database rps;
+use rps;
+
+create table if not exists player
+(
+    pk_playerID  int auto_increment primary key,
+    name         varchar(255),
+    symbolPlayed char(2)
+);
+
+create table if not exists game
+(
+    pk_gameID  int auto_increment primary key,
+    date       date,
+    fk_player1 int,
+    fk_player2 int,
+    foreign key (fk_player1) references player (pk_playerID),
+    foreign key (fk_player2) references player (pk_playerID)
+);
+
+insert into player(name, symbolPlayed)
+values ('moritz', 'sc'),
+       ('chrisi', 'pa'),
+       ('cyp', 'ro'),
+       ('gabriel', 'pa'),
+       ('nico', 'ro'),
+       ('hansi', 'sc');
+
+insert into game(date, fk_player1, fk_player2)
+values ('2021-7-25', 1, 2),
+       ('2021-3-31', 3, 4),
+       ('2023-2-12', 5, 6),
+       ('2022-10-5', 5, 2)
