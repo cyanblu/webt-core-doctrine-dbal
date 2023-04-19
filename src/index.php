47bd7d751c8794ee86bfdd165c34367578d276b4
Christian Widauer <9198@htl.rennweg.at>
2023-04-19 13:21:46 +0200

    halal

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "vendor/autoload.php";
+require_once "../vendor/autoload.php";
 
 use Doctrine\DBAL\DriverManager;
 
@@ -14,36 +14,14 @@ $connectionParams = [
 ];
 $conn = DriverManager::getConnection($connectionParams);
 
-$sql = "SELECT * FROM games";
-$sql2 = "SELECT * FROM player";
-$stmt = $conn->query($sql);
-$stmt2 = $conn->query($sql2);
+$player = $conn->createQueryBuilder();
+$player
+    ->select('name', 'symbolPlayed')
+    ->from('player');
 
-$stmt->execute();
-$stmt2->execute();
-$game = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$player = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+$game = $conn->createQueryBuilder();
+$game
+    ->select('date', 'fk_player1', 'fk_player2')
+    ->from('game');
+$game->fetchAllAssociative($player);
 
-// Display game round data in HTML table
-echo '<table>';
-echo '<tr><th>Player</th><th>Symbol</th><th>Date and Time</th></tr>';
-
-
-foreach ($game as $row) {
-    foreach ($player as $symbol) {
-        echo '<tr>';
-        echo '<td>' . $row['fk_player1'] . '</td>';
-        echo '<td>' . $row['fk_player2'] . '</td>';
-        echo '</tr>';
-        echo '<tr>';
-        if($row['fk_player1'] == $symbol['name']) {
-            echo '<td>' . $symbol['symbolPlayed'] . '</td>';
-        }
-        if($row['fk_player1'] == $symbol['name']) {
-            echo '<td>' . $symbol['symbolPlayed'] . '</td>';
-        }
-        echo '</tr>';
-    }
-}
-
-echo '</table>';
