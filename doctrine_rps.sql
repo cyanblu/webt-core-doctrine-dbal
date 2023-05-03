f22e00d0c630964d26f2145bb41504aa6bea89ba
Moritz Liedl <9186@htl.rennweg.at>
2023-05-03 12:22:00 +0200

    database updated

--- doctrine_rps.sql
+++ doctrine_rps.sql
@@ -12,7 +12,7 @@ create table if not exists player
 create table if not exists game
 (
     pk_gameID  int auto_increment primary key,
-    date       date,
+    datum       timestamp,
     fk_player1 int,
     fk_player2 int,
     foreign key (fk_player1) references player (pk_playerID),
@@ -27,8 +27,9 @@ values ('moritz', 'sc'),
        ('nico', 'ro'),
        ('hansi', 'sc');
 
-insert into game(date, fk_player1, fk_player2)
+insert into game(datum, fk_player1, fk_player2)
 values ('2021-7-25', 1, 2),
        ('2021-3-31', 3, 4),
        ('2023-2-12', 5, 6),
-       ('2022-10-5', 5, 2)
+       ('2022-10-5', 5, 2),
+       ('2022-10-5', 2, 6)
