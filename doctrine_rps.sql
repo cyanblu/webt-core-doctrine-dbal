cb2648143d1d60206725d876df74809d6cceba7d
ChristianWidauer <9198@htl.rennweg.at>
2023-05-16 21:52:42 +0200

    add fgehlt noch

--- doctrine_rps.sql
+++ doctrine_rps.sql
@@ -2,36 +2,33 @@ drop database if exists rps;
 create database rps;
 use rps;
 
-create table if not exists player
+create table if not exists symbol
 (
-    pk_playerID  int auto_increment primary key,
-    name         varchar(255),
+    pk_symbolID  int auto_increment primary key,
     symbolPlayed char(2)
     );
 
 create table if not exists game
 (
     pk_gameID  int auto_increment primary key,
-    datum       timestamp,
-    fk_player1 int,
-    fk_player2 int,
-    foreign key (fk_player1) references player (pk_playerID),
-    foreign key (fk_player2) references player (pk_playerID)
+    datum      timestamp,
+    player1    varchar(255),
+    player2    varchar(255),
+    fk_symbol1 int,
+    fk_symbol2 int,
+    foreign key (fk_symbol1) references symbol (pk_symbolID),
+    foreign key (fk_symbol2) references symbol (pk_symbolID)
     );
 
-insert into player(name, symbolPlayed)
-values ('Moritz', 'sc'),
-       ('Chrisi', 'pa'),
-       ('Cyp', 'ro'),
-       ('Gabriel', 'pa'),
-       ('Nico', 'ro'),
-       ('Hansi', 'sc');
+insert into symbol(symbolPlayed)
+values ('ro'),
+       ('pa'),
+       ('sc');
 
-insert into game(datum, fk_player1, fk_player2)
-values ('2021-7-25 12:00:00', 1, 2),
-       ('2021-3-31 12:00:00', 3, 4),
-       ('2023-2-12 12:00:00', 5, 6),
-       ('2022-10-5 12:00:00', 5, 2),
-       ('2022-10-5 12:00:00', 2, 6),
-       ('2022-09-5 12:00:00', 2, 4)
-
+insert into game(datum, player1, player2, fk_symbol1, fk_symbol2)
+values ('2021-7-25 12:00:00', 'Moritz', 'Chrisi', 1, 2),
+       ('2021-3-31 12:00:00', 'Cyp', 'Gabriel', 3, 1),
+       ('2023-2-12 12:00:00', 'Nico', 'Hansi', 2, 2),
+       ('2022-10-5 12:00:00', 'Nico', 'Chrisi', 3, 2),
+       ('2022-10-5 12:00:00', 'Chrisi', 'Hansi', 3, 3),
+       ('2022-09-5 12:00:00', 'Chrisi', 'Gabriel', 2, 1)
--- read_DB.php
+++ read_DB.php
@@ -35,12 +35,12 @@ $queryBuilderGame = $conn->createQueryBuilder();
 $queryBuilderSymbol = $conn->createQueryBuilder();
 
 $queryBuilderGame
-    ->select('datum', 'fk_player1', 'fk_player2')
+    ->select('pk_gameID', 'datum', 'player1', 'player2', 'fk_symbol1', 'fk_symbol2')
     ->from('game');
 
 $queryBuilderSymbol
     ->select('symbolPlayed')
-    ->from('player');
+    ->from('symbol');
 
 $symbols = $queryBuilderSymbol->fetchAllAssociative();
 $game = $queryBuilderGame->fetchAllAssociative();
@@ -49,6 +49,7 @@ $game = $queryBuilderGame->fetchAllAssociative();
 echo '<h2>RPS Game Tournament on 3rd of Mai, 2023</h2>';
 echo '<table>';
 echo '<tr>
+    <th>Delete Entry</th>
     <th>Player 1</th>
     <th>Symbol 1</th>
     <th>Player 2</th>
@@ -58,17 +59,22 @@ echo '<tr>
   </tr>';
 foreach ($game as $gameTable) {
     echo '<tr>';
-    echo '<td><input type="submit" name="button1"
-                 value="deleteRow" /></td>';
-    echo '<td>' . $gameTable['fk_player1'] . '</td>';
-    echo '<td>' . $gameTable['fk_player2'] . '</td>';
+    echo '<td><form method="post" action="' . $_SERVER['PHP_SELF'] . '">
+    <input type="hidden" name="gameId" value="' . $gameTable['pk_gameID'] . '">
+    <input type="submit" name="button1" value="Delete">
+</form></td>';
+    echo '<td>' . $gameTable['pk_gameID'] . '</td>';
+    echo '<td>' . $gameTable['player1'] . '</td>';
+    echo '<td>' . $symbols[$gameTable['fk_symbol1'] - 1]['symbolPlayed'] . '</td>';
+    echo '<td>' . $gameTable['player2'] . '</td>';
+    echo '<td>' . $symbols[$gameTable['fk_symbol2'] - 1]['symbolPlayed'] . '</td>';
     echo '<td>' . $gameTable['datum'] . '</td>';
-    echo '<td>' . gameResult($symbols[$gameTable['fk_player1'] - 1]['symbolPlayed'], $symbols[$gameTable['fk_player2'] - 1]['symbolPlayed'],
-            $symbols[$gameTable['fk_player1'] - 1]['name'], $player[$gameTable['fk_player2'] - 1]['name']) . '</td>';
+    echo '<td>' . gameResult($symbols[$gameTable['fk_symbol1'] - 1]['symbolPlayed'], $symbols[$gameTable['fk_symbol2'] - 1]['symbolPlayed'],
+            $gameTable['player1'], $gameTable['player2']) . '</td>';
     echo '</tr>';
 }
 echo '</table>';
-echO'<br><br>
+echo '<br><br>
 <h1>Insert new records in Database</h1>
     <h4>You may only need the first 2 letters of players and symbols</h4>
     <form action="create_del_DB.php" method="post">
@@ -99,3 +105,23 @@ function gameResult($symbol1, $symbol2, $name1, $name2)
         return $name2;
     }
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['button1'])) {
+        $gameId = $_POST['gameId'];
+        deleteGame($queryBuilderGame, $gameId);
+        header('Location: ' . $_SERVER['PHP_SELF']);
+        exit();
+    }
+}
+function deleteGame($queryBuilder, $gameId)
+{
+    $queryBuilder
+        ->delete('game')
+        ->where('pk_gameID = :gameId')
+        ->setParameter('gameId', $gameId)
+        ->execute();
+
+}
+
+function addGame($queryBuilder, $game){}
