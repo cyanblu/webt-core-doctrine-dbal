fade6244cdffd862b816b975f28bcc8bb95ce98c
Cyprian Marek <cyprianmarek@gmail.com>
2023-05-16 15:35:55 +0200

    allg twig struktur ohne doctrine

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ require_once 'vendor/autoload.php'; // Load Twig
 $loader = new \Twig\Loader\FilesystemLoader('templates'); // Specify the directory where your templates are located
 $twig = new \Twig\Environment($loader); // Create a new Twig environment
 
-echo $twig->render('body.html.twig', ['gibbyVar' => 'gibbeey' ]); // Render the "body.html.twig" templates and output the resulting HTML
+echo $twig->render('body.html.twig', ['gibbyVar' => 'gibbeey', 'runden' => 'gibbeey'  ]); // Render the "body.html.twig" templates and output the resulting HTML
+echo $twig->render('body2.html.twig', ['gibbyVar' => 'gibbeey' ]); // Render the "body.html.twig" templates and output the resulting HTML
